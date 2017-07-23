95dec3300118faf71343c7073a178d821f297010
Jonathan <johnnyboycurtis@gmail.com>
2017-07-22 18:22:25 -0700

    added Chi squared test example

--- Module 1/contingency_table.R
+++ Module 1/contingency_table.R
@@ -4,8 +4,10 @@ study = matrix(data = c(21, 2,
                dimnames = list(c("surgery", "radiation"), 
                                c("controlled", "not controlled")))
 alpha = 0.05
-B = 4000
+B = 10^4
 
+
+## SOME SET UP
 x = study
 almost.1 <- 1 - 64 * .Machine$double.eps
 
@@ -13,11 +15,11 @@ almost.1 <- 1 - 64 * .Machine$double.eps
 n <- sum(study)
 sr <- rowSums(x)
 sc <- colSums(x)
-E <- outer(sr, sc, "*")/n
+E <- outer(sr, sc, "*")/n ## ORDER MATTERS
 dimnames(E) <- dimnames(study)
 
 
-myfun <- function(B, E, sr, sc){
+simulateChisq <- function(B, E, sr, sc){
     results = numeric(B)
     for(i in 1:B){
         dat = unlist(r2dtable(1, sr, sc))
@@ -31,17 +33,38 @@ myfun <- function(B, E, sr, sc){
 #setMETH()
 #tmp <- .Call(C_chisq_sim, sr, sc, B, E)
 #tmp <- .Call(stats:::C_chisq_sim, sr, sc, B, E, PACKAGE="stats")
-tmp <- myfun(B, E, sr, sc)
+tmp <- simulateChisq(B, E, sr, sc)
 STATISTIC <- sum(sort((x - E)^2/E, decreasing = TRUE))
 PARAMETER <- NA
 PVAL <- (1 + sum(tmp >= almost.1 * STATISTIC))/(B + 1)
+
+
+ChisqTest <- function(data, Simulations){
+    ## data should be a 2X2 matrix
+    x = data
+    B = Simulations
+    #almost.1 <- 1 - 64 * .Machine$double.eps
+    n <- sum(x)
+    sr <- rowSums(x)
+    sc <- colSums(x)
+    E <- outer(sr, sc, "*")/n ## ORDER MATTERS
+    dimnames(E) <- dimnames(study)
+    tmp <- simulateChisq(B, E, sr, sc)
+    Stat <- sum(sort((x - E)^2/E, decreasing = TRUE))
+    pval <- (1 + sum(tmp >= almost.1 * STATISTIC))/(B + 1)
+    df = 2 ## only option for this example
+    rawPVal = pchisq(q = Stat, df = df, lower.tail = FALSE)
+    out = list(PearsonStat = Stat, MonteCarloPVal = pval, rawPVal = rawPVal)
+    return(out)
+}
+
 
 plot(density(tmp))
 print(STATISTIC)
 print(PVAL)
 
 
-w = chisq.test(study, simulate.p.value=TRUE)
+w = chisq.test(study, simulate.p.value=TRUE, B = B)
 print(w)
 
 
@@ -51,7 +74,9 @@ print(w)
 
 library(DescTools)
 
-GTest(study)
+G_test_stat = GTest(study)
+print("GTest:")
+print(G_test_stat)
 
 GStat <- function(x){
   ## x should be a matrix
@@ -68,5 +93,22 @@ GStat <- function(x){
   return(G)
 }
 
+print("GStat: ")
+print(GStat(study))
 
-GStat(study)
+
+
+simulateLogLike <- function(B, E, sr, sc){
+    results = numeric(B)
+    for(i in 1:B){
+        dat = unlist(r2dtable(1, sr, sc))
+        O = matrix(dat, ncol = length(sc), nrow = length(sr))
+        G = 2*( (O[1,1]*log(O[1,1]/E[1,1])) + 
+            (O[1,2]*log(O[1,2]/E[1,2])) + 
+            (O[2,1]*log(O[2,1]/E[2,1])) + 
+            (O[2,2]*log(O[2,2]/E[2,2]))  )
+        results[i] = G
+    }
+    return(results)
+}
+
