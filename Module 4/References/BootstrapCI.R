959cb56b6097bb44acfb0d6366b35b450f297e82
Jonathan <johnnyboycurtis@gmail.com>
2017-09-03 14:20:29 -0700

    updating slides

--- Module 4/References/BootstrapCI.R
+++ Module 4/References/BootstrapCI.R
@@ -9,9 +9,9 @@ h = function(x){
 
 x = matrix(data = h(runif(200*n)), ncol = 200)
 estint = apply(x, 2, cumsum) / (1:n)
-
-
 y = apply(estint, 1, quantile, c(0.025, 0.975))
+
+plot(estint[,1], type = "l", col = 2, ylim = c(0.8, 1.2), xlim = c(0, 10000))
 polygon(c(1:n, n:1), c(y[1,], rev(y[2,])), col = "gray90")
 lines(estint[,1], type = "l", col = 2, ylim = c(0.8, 1.2))
 
@@ -31,4 +31,9 @@ lines(bootL, col = 4)
 z = runif(n)
 lines(cumsum(h(z))/1:n, col = 5)
 
- 
+
+
+plot(x = 1:n, y = theta_est, type = "l", lty=2, ylim = c(0.7, 1.2), xlim = c(0, 10000))
+lines(x = 1:n, y = parallelCI[,1], col = "blue")
+lines(x = 1:n, y = parallelCI[,3], col = "blue")
+lines(x = 1:n, y = cumsum(h(z))/1:n, col = "red", lty = 3)
